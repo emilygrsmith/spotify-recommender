f9e706236bbcb7d22a48f836955b8d944b41faed
emilygrsmith <emilygrsmith11@gmail.com>
2023-07-18 23:03:17 -0400

    no duplicates

--- src/app/load_data.py
+++ src/app/load_data.py
@@ -11,13 +11,7 @@ import pickle
 
 load_dotenv()
 
-PUBLIC_KEY = os.getenv('CLIENT_PUBLIC_KEY')
-SECRET_KEY = os.getenv('CLIENT_SECRET_KEY')
 
-client_credentials_manager = SpotifyClientCredentials(client_id=PUBLIC_KEY, client_secret=SECRET_KEY)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-
-ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
 class Playlist:
@@ -25,48 +19,72 @@ class Playlist:
         self._playlistID = id
         self._length = 0
         self._playlistInfo = []
+        self._PUBLIC_KEY = os.getenv('CLIENT_PUBLIC_KEY')
+        self._SECRET_KEY = os.getenv('CLIENT_SECRET_KEY')
+        self._ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+        self._client_credentials_manager = SpotifyClientCredentials(client_id=self._PUBLIC_KEY, client_secret=self._SECRET_KEY)
+        self._sp = spotipy.Spotify(client_credentials_manager = self._client_credentials_manager)
         self._token = self._acquire_token()
         self._info = self._loadPlayListInfo()
+        self._uri = []
+
+       
         
     def _loadPlayListInfo(self):
-        info = sp.playlist_items(self._playlistID)
+        info = self._sp.playlist_items(self._playlistID)
         self._length = len(info['items'])
         return info
+    def _sendRequest(self, request):
+        
+            response =requests.get(request, 
+                headers={"Content-Type":"application/json", 
+                            "Authorization":f"Bearer {self._token}"})
+            return response.json()
+
     def getRecomend(self,filters = None):
         uris = []
         endpoint_url = "https://api.spotify.com/v1/recommendations?"
         
         for x in range(self._length):
-            query = f"{endpoint_url}limit={1}&market=US&seed_tracks={self._info['items'][x]['track']['id']}"
-            response =requests.get(query, 
-                headers={"Content-Type":"application/json", 
-                            "Authorization":f"Bearer {self._token}"})
             
-            json_response = response.json()
+            query = f"{endpoint_url}limit={1}&market=US&seed_tracks={self._info['items'][x]['track']['id']}"
+            json_response = self._sendRequest(query)
             #print((json_response))
             for i in json_response['tracks']:
+                name = i['name']
+                new_request = i
                 print(i['name'])
-                uris.append(i['name'])
+                while(name in uris):
+                    new_request = self._sendRequest(query)
+                    name = new_request['tracks']['name']
+                
+                    
+                self._uri.append(new_request)
+                uris.append(name)
         #print(len(uris))            
         return uris
-
-
+    def getUserID(self):
+        user_info = self._sp.current_user()
+        print(user_info)
+        return user_info
+    def buildPlaylist(self):
+        self._getUserID()
     def _acquire_token(self):
         print("ACQUIRING TOKEN")
-        token_cache_dir = os.path.join(ROOT_PATH, "cache")
+        token_cache_dir = os.path.join( self._ROOT_PATH, "cache")
         token_cache_file = os.path.join(token_cache_dir, "token.p")
         if os.path.exists(token_cache_file):
             current_time = time.time()
-            # if current_time - os.path.getmtime(token_cache_file) < 3600:
-            #     with open(token_cache_file, "rb") as fid:
-            #         token = pickle.load(fid)
-            #         print("t")
-            #     return token
-            if(True):
+            if current_time - os.path.getmtime(token_cache_file) < 3600:
+                with open(token_cache_file, "rb") as fid:
+                    token = pickle.load(fid)
+                    print("t")
+                return token
+            else:
                 grant_type = 'client_credentials'
                 body_params = {'grant_type' : grant_type}
                 url = 'https://accounts.spotify.com/api/token'
-                response = requests.post(url, data=body_params, auth=(PUBLIC_KEY, SECRET_KEY))
+                response = requests.post(url, data=body_params, auth=(self._PUBLIC_KEY, self._SECRET_KEY))
                 token_raw = json.loads(response.text)
                 token = token_raw["access_token"]
                 with open(token_cache_file, "wb") as fid:
@@ -77,7 +95,7 @@ class Playlist:
             grant_type = 'client_credentials'
             body_params = {'grant_type' : grant_type}
             url = 'https://accounts.spotify.com/api/token'
-            response = requests.post(url, data=body_params, auth=(PUBLIC_KEY, SECRET_KEY))
+            response = requests.post(url, data=body_params, auth=(self.__KEY, self._SECRET_KEY))
             token_raw = json.loads(response.text)
             token = token_raw["access_token"]
             with open(token_cache_file, "wb") as fid:
@@ -86,7 +104,8 @@ class Playlist:
             return token
 if __name__ == "__main__":
     p = Playlist('3KkQ3MvqPUtnXSZQq08SOv')
-    li = p.getRecomend()
+    #li = p.getRecomend()
+    print( p.getUserID())
     
 
     
